243b50c7faf02d2a9dd5d9843b3a259dee3d5dc1
LingGrani <luthfiangger2510@gmail.com>
2023-05-15 17:06:38 +0700

    desctrution

--- destruktor_137/destruktor_137.cpp
+++ destruktor_137/destruktor_137.cpp
@@ -17,3 +17,11 @@ angka::angka(int i) {
 	arr = new int[i];
 	isiData();
 }
+
+angka::~angka() {//destruction
+	cout << endl;
+	cetakData();
+	delete[]arr;
+	cout << "Alamat Array Sudah Dilepaskan" << endl;
+}
+
